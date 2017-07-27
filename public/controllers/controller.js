273f7f3f14892a6c4eab5482a24731d7584b06c8
mounika <mounika.rgula@gmail.com>
2017-07-27 17:07:49 -0500

    created dummy data to test the table using controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,26 @@
-function AppCtrl() {
+function AppCtrl($scope) {
 	console.log("hello")
+
+	person1 = {
+		name: "mounika",
+		email: "mounika@gmail.com",
+		number: "814-384-6835"
+	};
+
+
+	person2 = {
+		name: "vishal",
+		email: "vishal@gmail.com",
+		number: "813-335-5928"
+	};
+
+	person3 = {
+		name: "varshika",
+		email: "varsh@gmail.com",
+		number: "966-633-6176"
+	};
+
+	var contactlist =[person1, person2, person3]
+	$scope.contactlist = contactlist;
+
 }
