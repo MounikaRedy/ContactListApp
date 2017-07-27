7bffc2ab71b3f98688050ca453799d62b0d95c34
mounika <mounika.rgula@gmail.com>
2017-07-27 17:19:27 -0500

    formatted dummy data in contactlist app using ng-repeat

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,22 +2,22 @@ function AppCtrl($scope) {
 	console.log("hello")
 
 	person1 = {
-		name: "mounika",
-		email: "mounika@gmail.com",
-		number: "814-384-6835"
+		Name: "mounika",
+		Email: "mounika@gmail.com",
+		Number: "814-384-6835"
 	};
 
 
 	person2 = {
-		name: "vishal",
-		email: "vishal@gmail.com",
-		number: "813-335-5928"
+		Name: "vishal",
+		Email: "vishal@gmail.com",
+		Number: "813-335-5928"
 	};
 
 	person3 = {
-		name: "varshika",
-		email: "varsh@gmail.com",
-		number: "966-633-6176"
+		Name: "varshika",
+		Email: "varsh@gmail.com",
+		Number: "966-633-6176"
 	};
 
 	var contactlist =[person1, person2, person3]
