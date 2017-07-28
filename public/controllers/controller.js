174a02421f49f65dc636b3698e4b7bd06f28b549
mounika <mounika.rgula@gmail.com>
2017-07-27 21:46:55 -0500

    modifications made to both the controller and server.js file to get the data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,28 +1,9 @@
 function AppCtrl($scope, $http) {
 	console.log("hello");
 
-	$http.get('/contactlist')
-
-	person1 = {
-		Name: "mounika",
-		Email: "mounika@gmail.com",
-		Number: "814-384-6835"
-	};
-
-
-	person2 = {
-		Name: "vishal",
-		Email: "vishal@gmail.com",
-		Number: "813-335-5928"
-	};
-
-	person3 = {
-		Name: "varshika",
-		Email: "varsh@gmail.com",
-		Number: "966-633-6176"
-	};
-
-	var contactlist =[person1, person2, person3]
-	$scope.contactlist = contactlist;
+	$http.get('/contactlist').success(function(response) {
+		console.log("I got the data I requested");
+		$scope.contactlist = response;
+	});
 
 }
--- server.js
+++ server.js
@@ -5,6 +5,28 @@ app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function(req, res) {
 	console.log("I received a GET request")
+
+	person1 = {
+		Name: "mounika",
+		Email: "mounika@gmail.com",
+		Number: "814-384-6835"
+	};
+
+
+	person2 = {
+		Name: "vishal",
+		Email: "vishal@gmail.com",
+		Number: "813-335-5928"
+	};
+
+	person3 = {
+		Name: "varshika",
+		Email: "varsh@gmail.com",
+		Number: "966-633-6176"
+	};
+
+	var contactlist =[person1, person2, person3];
+	res.json(contactlist);
 });
 
 app.listen(3000);
